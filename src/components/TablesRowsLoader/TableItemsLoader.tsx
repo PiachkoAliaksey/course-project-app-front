5116f74fa8c99dbeafaee6c90634f3872d45bab2
PiachkoAliaksey <alek17y@mail.ru>
2023-06-29 20:11:20 +0300

    add: Skeleton for items table

---
+++ src/components/TablesRowsLoader/TableItemsLoader.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import { Skeleton } from "@mui/material";
+import "../Table/Table.scss";
+
+
+
+export const TableItemLoader:React.FC<{rowsNum:number}> = ({ rowsNum }) => {
+  return (
+    <>
+    {[...Array(rowsNum)].map((_, index) => (
+      <TableRow key={index}>
+        <TableCell>
+          <Skeleton animation="wave" variant="text" />
+        </TableCell>
+        <TableCell>
+          <Skeleton animation="wave" variant="text" />
+        </TableCell>
+        <TableCell>
+          <Skeleton animation="wave" variant="text" />
+        </TableCell>
+        <TableCell>
+          <Skeleton animation="wave" variant="text" />
+        </TableCell>
+      </TableRow>
+    ))}
+    </>
+
+
+  );
+};
