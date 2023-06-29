63b523c53a2c9236489c5d59818789d74e8e5e92
PiachkoAliaksey <alek17y@mail.ru>
2023-06-29 19:54:51 +0300

    refactor: store redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,15 @@ import { Store } from "redux"
 import { configureStore} from "@reduxjs/toolkit";
 
 
+
 import { authReducer } from "./slices/auth";
+import {usersTableReducer} from "./slices/usersTable"
+import {collectionReducer} from "./slices/collection"
+import {allCollectionReducer} from "./slices/allCollections"
+import {itemReducer} from "./slices/item"
+import { allCollectionItemsReducer } from "./slices/allCollectionItems";
+import {commentReducer} from './slices/comment';
+import {allCommentsReducer} from './slices/allComments';
 
 
 
@@ -10,7 +18,17 @@ import { authReducer } from "./slices/auth";
 export const store:Store = configureStore({
   reducer: {
     auth:authReducer,
-  }
+    users:usersTableReducer,
+    collection:collectionReducer,
+    collections:allCollectionReducer,
+    item:itemReducer,
+    items:allCollectionItemsReducer,
+    comment:commentReducer,
+    allComments:allCommentsReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+    {serializableCheck: false}
+  )
 })
 
 export type RootState = ReturnType<typeof store.getState>
