c271136ad3a6af3e8ac1062d3950551e58c794e7
PiachkoAliaksey <alek17y@mail.ru>
2023-06-29 20:13:50 +0300

    add: Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-
-import './Header.scss';
-import Container from '@mui/material/Container';
-import { TextField, InputAdornment, Avatar } from "@mui/material";
-import { Typography } from '@mui/material';
-import SearchIcon from "@mui/icons-material/Search";
-
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
+import { useTranslation } from "react-i18next";
+
+import './Header.scss';
+
+import Button from '@mui/material/Button';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import { TextField, InputAdornment, Avatar } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import LightMode from '@mui/icons-material/LightMode';
+import ModeNight from '@mui/icons-material/ModeNight';
+import { Typography } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import Divider from '@mui/material/Divider';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+
 import { logout } from '../../redux/slices/auth';
 import { stringAvatar } from '../../utils/createNameAvatar';
+import { ColorModeContext } from '../../App';
+import { LANGUAGES } from '../../constant/index';
+
+
 
 
 export const Header = () => {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+  const { i18n, t } = useTranslation();
+
+
   const dispatch = useDispatch();
   const userData = useSelector((state: RootState) => state.auth.userData);
   const isLoading = userData.status === 'loading';
   const isAuth = Boolean(userData.data);
+  //const isAdmin = userData.data.position === 'admin'
+
+  const [language, setLanguage] = useState<string>(JSON.parse(localStorage.getItem('language')!) || 'en');
+  const handleChangeLanguage = (event: SelectChangeEvent) => {
+    const lang_code = event.target.value;
+    i18n.changeLanguage(lang_code);
+    setLanguage(event.target.value);
+  };
 
   const onClickLogout = () => {
     if (window.confirm('Are you sure you want to logout'))
@@ -26,50 +55,85 @@ export const Header = () => {
     window.localStorage.removeItem('token')
   };
 
+  useEffect(() => {
+    localStorage.setItem('language', JSON.stringify(language));
+  }, [language]);
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, []);
+
   return (
-    <div className="nav-bar">
-      <Container maxWidth="lg">
+    <>
+      <Container maxWidth={false} sx={{ padding: '15px', bgcolor: 'background.default', color: 'text.primary' }}>
         <div className="inner">
           <Link className="logo" to="/">
-            <div>Self Collection</div>
+            <Typography variant='h4'>{t("title")}</Typography>
           </Link>
-
-            <TextField
-              id="search"
-              type="search"
-              label="Search"
-
-              sx={{ width: 300 }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
+          <TextField
+            id="search"
+            type="search"
+            label={t("search")}
+            className='search-bar'
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <FormControl sx={{ m: 1, minWidth: 80, marginBottom:'5px' }} size="small">
+            <InputLabel>Language</InputLabel>
+            <Select
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-select-small"
+              value={language}
+              label={t("language")}
+              onChange={handleChangeLanguage}
+              displayEmpty
+            >
+              {LANGUAGES.map(({ label, code }) => <MenuItem key={code} value={code}>{label}</MenuItem>)}
+            </Select>
+          </FormControl>
+          <IconButton sx={{ ml: 1, marginBottom:'5px'}} onClick={colorMode.toggleColorMode} color="inherit">
+            {theme.palette.mode === 'dark' ? <LightMode /> : <ModeNight />}
+          </IconButton>
 
           <div className="buttons">
-            {isAuth ? (
-              <>
-                {isLoading ? '': <Avatar  {...stringAvatar(userData.data.fullName)} sx={{ width: 50, height: 50 }}/>}
-                <Button onClick={onClickLogout} variant="contained" color="error">
-                  Log out
-                </Button>
-              </>
+            {isAuth ? (userData.data.position === 'admin' ? (<>
+              {isLoading ? '' : <Avatar  {...stringAvatar(userData.data.fullName)} className='avatar-user' />}
+              <Link to="/adminpanel">
+                <Button variant="outlined">Admin panel</Button>
+              </Link>
+              <Button onClick={onClickLogout} variant="contained" color="error">
+                {t("logout")}
+              </Button>
+
+            </>) : (<>
+              {isLoading ? '' : <Avatar  {...stringAvatar(userData.data.fullName)} className='avatar-user' />}
+              <Link to="/collection">
+                <Button variant="outlined">Create collection</Button>
+              </Link>
+              <Button onClick={onClickLogout} variant="contained" color="error">
+                {t("logout")}
+              </Button>
+
+            </>)
+
             ) : (
               <>
                 <Link to="/login">
-                  <Button variant="outlined">Log in</Button>
+                  <Button variant="outlined">{t("login")}</Button>
                 </Link>
                 <Link to="/signup">
-                  <Button variant="contained">Sign up</Button>
+                  <Button variant="contained">{t("signup")}</Button>
                 </Link>
               </>
             )}
           </div>
         </div>
       </Container>
-    </div>
+      <Divider />
+    </>
   );
 };
