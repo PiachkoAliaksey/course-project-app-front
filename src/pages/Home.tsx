e35401e43ab9ff16082fd76e26a2cd2afbd7488a
PiachkoAliaksey <alek17y@mail.ru>
2023-07-09 20:54:58 +0300

    refactor: Home page, skeleton component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,7 +28,7 @@ import { fetchUserLargestCollection } from '../redux/slices/allCollections';
 import { IItem } from '../redux/slices/item';
 import { ICollection } from '../redux/slices/collection';
 import { SkeletonListLoader } from '../components/TablesRowsLoader/SkeletonListLoader';
-
+import { SkeletonListFiveLargestCollection } from '../components/TablesRowsLoader/SkeletonListFiveLargestCollection'
 import './Home.scss'
 
 
@@ -53,6 +53,7 @@ export const Home: React.FC = () => {
   const collectionsData: ICollection[] = useSelector((state: RootState) => state.collections.allCollections.items);
   const isLoadedCollections = Boolean(collectionsData);
   const isLoadedItemsData = Boolean(items.allCollectionItems);
+  console.log(isLoadedItemsData && items.allCollectionItems)
   const isLoadedTags = Boolean(allTags.tags);
   const isLoadingUser = userData.status === 'loading';
   const isAuth = Boolean(userData.data);
@@ -71,24 +72,29 @@ export const Home: React.FC = () => {
 
   return (
     <Grid container spacing={4}>
-      <Grid xs={9} item>
+      <Grid xs={12} item>
+        <List classes={{ root: 'list-of-tags' }}>
+          {isLoadedTags && allTags.tags.map((tag: string[]) => <Link key={uuidv4()} className='link-tag' to={`/search/${tag.slice(1)}`}><ListItemButton sx={{ color: 'text.primary' }} key={uuidv4()}>{tag}</ListItemButton></Link>)}
+        </List>
+      </Grid>
+      <Grid classes={{root:'main-container-items-collections'}} xs={12} item>
         <Box sx={{ padding: '10px', borderRadius: '5px', boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', marginBottom: '10px' }}>
-          <Typography variant='h4'>List of last added items</Typography>
+          <Typography classes={{root:'title-main-container-items-collections'}} variant='h5'>{t("listLastItems")}</Typography>
           <List>
             <ListItem>
               <Grid container classes={{ root: 'title-list-five-last-items' }}>
-                <Grid item xs={4}>Title</Grid>
-                <Grid item xs={4}>Collection</Grid>
-                <Grid item xs={4}>User</Grid>
+                <Grid item xs={4}>{t("titleItem")}</Grid>
+                <Grid item xs={4}>{t("collection")}</Grid>
+                <Grid item xs={4}>{t("user")}</Grid>
               </Grid>
             </ListItem>
             {isLoadedItemsData ? (items.allCollectionItems.map((obj: IItem) =>
               <Link key={obj._id} className='link-item-last-five' to={`/collection/items/item/${obj._id}`}>
-                <ListItemButton >
-                  <Grid container>
-                    <Grid item xs={4}>{obj.title}</Grid>
-                    <Grid item xs={4}>{obj.collectionName}</Grid>
-                    <Grid item xs={4}>{obj.user}</Grid>
+                <ListItemButton sx={{ color: 'text.primary' }} >
+                  <Grid container spacing={0.5}>
+                    <Grid classes={{ root: 'last-added-item' }} item xs={4}>{obj.title}</Grid>
+                    <Grid classes={{ root: 'last-added-item' }} item xs={4}>{obj.collectionName}</Grid>
+                    <Grid classes={{ root: 'last-added-item' }} item xs={4}>{obj.user}</Grid>
                   </Grid>
                 </ListItemButton>
               </Link>)) : (<SkeletonListLoader rowsNum={5} />)}
@@ -96,17 +102,13 @@ export const Home: React.FC = () => {
 
         </Box>
         <Box sx={{ padding: '10px', borderRadius: '5px', boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px' }}>
-          <Typography variant='h4'>List of 5 largest collection</Typography>
-          <List>
-            {isLoadedCollections && collectionsData.map((obj) => <ListItem key={uuidv4()}>{obj.title}</ListItem>)}
+          <Typography classes={{root:'title-main-container-items-collections'}} variant='h5'>{t("listLargestCollection")}</Typography>
+          <List >
+            {isLoadedCollections ? (collectionsData.map((obj) => <ListItem classes={{ root: 'last-added-item' }} key={uuidv4()}>{obj.title}</ListItem>)) : (<SkeletonListFiveLargestCollection itemNum={5} />)}
           </List>
         </Box>
       </Grid>
-      <Grid xs={3} item>
-        <List classes={{root:'list-of-tags'}}>
-          {isLoadedTags && allTags.tags.map((tag: string[]) => <Link to={`/search/${tag}`}><ListItemButton key={uuidv4()}>{tag}</ListItemButton></Link>)}
-        </List>
-      </Grid>
+
     </Grid>
   );
 };
