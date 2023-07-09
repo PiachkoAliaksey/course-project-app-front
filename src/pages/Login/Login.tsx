0269ff93f1297b58fed5fca8ce1e6499887c2402
PiachkoAliaksey <alek17y@mail.ru>
2023-07-09 21:02:24 +0300

    add: Login component, mui dialog for notation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,74 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
+import { Navigate } from "react-router-dom";
+import { AnyAction } from "redux";
+import { fetchUserData } from "../../redux/slices/auth";
+import { RootState } from "redux/store";
+
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import {useForm} from "react-hook-form";
-import { useDispatch,useSelector } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
-import { Navigate } from "react-router-dom";
-import { AnyAction } from "redux";
-import { fetchUserData} from "../../redux/slices/auth";
-import { RootState } from "redux/store";
+
 import "./Login.scss";
 
-export const Login:React.FC = () => {
+export const Login: React.FC = () => {
   const { i18n, t } = useTranslation();
-  const isAuth = useSelector((state:RootState) => Boolean(state.auth.userData.data)&&state.auth.userData.data.status!=='blocked')
-  const dispatch:ThunkDispatch<{email: string,password:string}, void, AnyAction> = useDispatch();
+  const isAuth = useSelector((state: RootState) => Boolean(state.auth.userData.data) && state.auth.userData.data.status !== 'blocked')
+  const dispatch: ThunkDispatch<{ email: string, password: string }, void, AnyAction> = useDispatch();
   const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
     defaultValues: {
       email: '',
       password: '',
     },
-    mode:'onChange'
+    mode: 'onChange'
   })
+  const [open, setOpen] = React.useState(false);
+  const [notCorrectData, setNotCorrectData] = useState(false);
+  const [statusBlocked, setStatusBlocked] = useState(false);
 
-  const onSubmit = async(values: { email: string, password: string }) => {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const onSubmit = async (values: { email: string, password: string }) => {
     console.log(values);
     const data = await dispatch(fetchUserData(values));
-    if(!data.payload){
-      return alert('Please, write Full Name or Password correct')
+    if (!data.payload) {
+      console.log('here')
+      setNotCorrectData(true);
+      setOpen(true);
+      // return alert()
     }
 
-    if(data.payload.status==='blocked'){
-      return alert('User is blocked')
+    if (data.payload.status === 'blocked') {
+      console.log('here')
+      setStatusBlocked(true);
+      setOpen(true);
+      // return alert('User is blocked')
     }
 
-    if('token' in data.payload){
-      window.localStorage.setItem('token',data.payload.token)
+    if ('token' in data.payload&&data.payload.status !== 'blocked') {
+      window.localStorage.setItem('token', data.payload.token)
     }
   }
 
-  if(isAuth){
+  if (isAuth) {
     return <Navigate to='/'></Navigate>
   }
 
   return (
-    <Paper classes={{ root: "login-bar"}}>
-      <Typography classes={{ root: "login-title" }} variant="h5">
-      {t("entertoaccount")}
-      </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          className="field"
-          label={t("eMail")}
-          type='email'
-          error={Boolean(errors.email?.message)}
-          helperText={errors.email?.message}
-          {...register('email', { required: 'Email' })}
-          fullWidth
-        />
-        <TextField className="field" label={t("password")}
-        error={Boolean(errors.password?.message)}
-        {...register('password', { required: 'Password' })}
-        helperText={errors.password?.message}
-        fullWidth />
-        <Button type="submit" size="large" variant="contained" fullWidth>
-          Enter
-        </Button>
-      </form>
-    </Paper>
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {statusBlocked && (<DialogTitle id="alert-dialog-title">
+          {t("userIsBlocked")}
+        </DialogTitle>)}
+        {notCorrectData&&(<DialogTitle id="alert-dialog-title">
+          {t("pleaseWriteFullNameOrPasswordCorrect")}
+        </DialogTitle>)}
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Paper classes={{ root: "login-bar" }}>
+        <Typography classes={{ root: "login-title" }} variant="h5">
+          {t("enterToAccount")}
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            className="field"
+            label={t("eMail")}
+            type='email'
+            error={Boolean(errors.email?.message)}
+            helperText={errors.email?.message}
+            {...register('email', { required: 'Email' })}
+            fullWidth
+          />
+          <TextField className="field" label={t("password")}
+            error={Boolean(errors.password?.message)}
+            {...register('password', { required: 'Password' })}
+            helperText={errors.password?.message}
+            fullWidth />
+          <Button type="submit" size="large" variant="contained" fullWidth>
+            {t("enter")}
+          </Button>
+        </form>
+      </Paper>
+    </>
+
   );
 };
