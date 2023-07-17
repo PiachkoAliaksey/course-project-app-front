089c78a25b5737f7e1e5dd7dbff4ce8d78afd29a
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:32:38 +0300

    refactor: redux strore

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,33 +1,33 @@
-import { Store } from "redux"
-import { configureStore} from "@reduxjs/toolkit";
+import { Store } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 
 
 import { authReducer } from "./slices/auth";
-import {usersTableReducer} from "./slices/usersTable"
-import {collectionReducer} from "./slices/collection"
-import {allCollectionReducer} from "./slices/allCollections"
-import {itemReducer} from "./slices/item"
+import { usersTableReducer } from "./slices/usersTable";
+import { collectionReducer } from "./slices/collection";
+import { allCollectionReducer } from "./slices/allCollections";
+import { itemReducer } from "./slices/item";
 import { allCollectionItemsReducer } from "./slices/allCollectionItems";
-import {commentReducer} from './slices/comment';
-import {allCommentsReducer} from './slices/allComments';
+import { commentReducer } from './slices/comment';
+import { allCommentsReducer } from './slices/allComments';
 
 
 
 
-export const store:Store = configureStore({
+export const store: Store = configureStore({
   reducer: {
-    auth:authReducer,
-    users:usersTableReducer,
-    collection:collectionReducer,
-    collections:allCollectionReducer,
-    item:itemReducer,
-    items:allCollectionItemsReducer,
-    comment:commentReducer,
-    allComments:allCommentsReducer
+    auth: authReducer,
+    users: usersTableReducer,
+    collection: collectionReducer,
+    collections: allCollectionReducer,
+    item: itemReducer,
+    items: allCollectionItemsReducer,
+    comment: commentReducer,
+    allComments: allCommentsReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(
-    {serializableCheck: false}
+    { serializableCheck: false }
   )
 })
 
