2ef7d24d9568f250685c205fe25d86e76c4325e7
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:32:04 +0300

    refactor: SearchPage page

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect, SyntheticEvent } from 'react';
-import { io, Socket } from 'socket.io-client'
 import { useParams } from "react-router-dom";
 import { AnyAction } from "redux";
 import { RootState } from "redux/store";
 import { ThunkDispatch } from "redux-thunk";
-import { Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
-import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { useTranslation } from "react-i18next";
