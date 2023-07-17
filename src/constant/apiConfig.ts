4e691d81102af7f9a59669075b4e6922897df939
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:25:58 +0300

    refactor: apiConfig component

--- src/constant/apiConfig.ts
+++ src/constant/apiConfig.ts
@@ -4,7 +4,8 @@ const REACT_APP_API_URL = 'http://localhost:4444';
 export const instance = axios.create({
   baseURL: REACT_APP_API_URL
 });
+
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem('token');
   return config;
-})
+});
