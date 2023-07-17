dcc133c1ff9cb0d1a36aeeb9b41657139276501e
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:34:58 +0300

    refactor: sliceReducer comment

--- src/redux/slices/comment.tsx
+++ src/redux/slices/comment.tsx
@@ -3,47 +3,47 @@ import { instance } from '../../constant/apiConfig';
 
 
 
-export const fetchAddComment = createAsyncThunk('message/fetchAddComment', async (params:{from:string,to:string,comment:string}) => {
-  const { data } = await instance.post('/collection/items/item/addComment',params);
+export const fetchAddComment = createAsyncThunk('message/fetchAddComment', async (params: { from: string, to: string, comment: string }) => {
+  const { data } = await instance.post('/collection/items/item/addComment', params);
   return data;
 })
 
 
 
 interface IComment {
-    messages: {
-      _id:string,
-      message:{},
-      users:string[],
-      sender:string,
-      createdAt:string,
-      updatedAt:string,
-      __v:number
-    }|{},
-    status: string
+  messages: {
+    _id: string,
+    message: {},
+    users: string[],
+    sender: string,
+    createdAt: string,
+    updatedAt: string,
+    __v: number
+  } | {},
+  status: string
 }
-const initialState = {
-    messages: {},
-    status: 'loading'
 
-}as IComment;
+const initialState = {
+  messages: {},
+  status: 'loading'
+} as IComment;
 
 const commentSlice = createSlice({
   name: 'comment',
   initialState,
   reducers: {},
-  extraReducers:(builder)=> {
+  extraReducers: (builder) => {
     builder.addCase(fetchAddComment.pending, (state) => {
-      state.messages={};
+      state.messages = {};
       state.status = 'loading';
     })
     builder.addCase(fetchAddComment.fulfilled, (state, action) => {
-      state.messages = action.payload
-      state.status = 'loaded'
+      state.messages = action.payload;
+      state.status = 'loaded';
     })
     builder.addCase(fetchAddComment.rejected, (state) => {
-      state.messages={};
-      state.status = 'error'
+      state.messages = {};
+      state.status = 'error';
     })
 
   }
