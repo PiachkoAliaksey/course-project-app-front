2dd8da2cb1328c4acc80897e2dc6d9e7d0b769c9
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 21:24:53 +0300

    refactor: files

--- src/constant/apiConfig.ts
+++ src/constant/apiConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REACT_APP_API_URL = 'http://localhost:4444';
+const REACT_APP_API_URL = 'https://self-collection-server.onrender.com';
 export const instance = axios.create({
   baseURL: REACT_APP_API_URL
 });
