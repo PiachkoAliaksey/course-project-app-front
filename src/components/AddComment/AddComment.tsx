d8aa77edc54361cb4e19fb0dfd73b157f801d9f1
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:24:51 +0300

    refactor: AddComment component

--- src/components/AddComment/AddComment.tsx
+++ src/components/AddComment/AddComment.tsx
@@ -7,15 +7,13 @@ import { Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
-import { io, Socket } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client';
 
 import "./AddComment.scss";
 
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-
-
 
 import { IUser } from '../../pages/AdminPanel/AdminPanel';
 import { IItem } from '../../redux/slices/item';
@@ -44,12 +42,7 @@ export const AddComments:React.FC<IAddComment> = ({itemIndex,socket,setCommentsU
   const userData: IUser = useSelector((state: RootState) => state.auth.userData.data);
 
   const isAuth = Boolean(userData);
-
-
   const [comment, setComment] = useState<string>('');
-
-
-
 
   const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
     values: {
@@ -67,16 +60,10 @@ export const AddComments:React.FC<IAddComment> = ({itemIndex,socket,setCommentsU
         message: text.comment,
         senderUser: userData.fullName
       });
-      // const today = new Date();
-      // const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      // const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-      // const month = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
       setCommentsUsers([...commentsUsers,{ from:userData.fullName, message: text.comment, created: date }]);
       setComment('');
     }
   }
-
-
 
   return (
     <>
