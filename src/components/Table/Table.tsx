7915aa3ed20fbf7372c51cb02d47eb8605176520
PiachkoAliaksey <alek17y@mail.ru>
2023-07-19 21:27:21 +0300

    refactor: Table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,16 +1,11 @@
-import React, { useState, SetStateAction, Dispatch } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from "react-router-dom";
+import React from "react";
+
 import { Link } from 'react-router-dom';
 import { RootState } from 'redux/store';
-import { AnyAction } from 'redux';
-import { ThunkDispatch } from 'redux-thunk';
-
 
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import { Checkbox } from "@mui/material";
-import FormControlLabel from '@mui/material/FormControlLabel';
 import { v4 as uuidv4 } from 'uuid';
 
 import { IUser } from "../../pages/AdminPanel/AdminPanel";
@@ -22,44 +17,35 @@ interface ITable {
   createdAt: string
   updatedAt: string
   status: string,
-  position:string,
-  index:number,
-  isCheck:string[],
-  setIsCheck:React.Dispatch<React.SetStateAction<string[]>>
+  position: string,
+  index: number,
+  isCheck: string[],
+  setIsCheck: React.Dispatch<React.SetStateAction<string[]>>
 }
 
 
-export const UsersTable: React.FC<ITable> = ({setIsCheck,isCheck,index, _id, fullName, email, createdAt, updatedAt, status,position }) => {
+export const UsersTable: React.FC<ITable> = ({ setIsCheck, isCheck, index, _id, fullName, email, createdAt, updatedAt, status, position }) => {
   const userData: { data: IUser, status: string } = useSelector((state: RootState) => state.auth.userData);
   const isAuth = Boolean(userData.data);
 
   const handlerChangeCheckBox = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, checked } = e.target;
     if (e.target.checked) {
-      setIsCheck([...isCheck,id])
+      setIsCheck([...isCheck, id])
     } else {
       setIsCheck(isCheck.filter(item => item !== id));
     }
   }
 
   return (
-    <React.Fragment key={uuidv4()}>{isAuth&&userData.data._id===_id?(<TableRow key={uuidv4()}>
-    <TableCell component="th" scope="row">
-    <Checkbox  id={_id} checked={isCheck.includes(_id)}  onChange={handlerChangeCheckBox} />
-    </TableCell>
-    <TableCell>{fullName}</TableCell>
-    <TableCell>{email}</TableCell>
-    <TableCell>{position}</TableCell>
-    <TableCell>{status}</TableCell>
-  </TableRow>):(<TableRow key={uuidv4()}>
-    <TableCell component="th" scope="row">
-    <Checkbox  id={_id} checked={isCheck.includes(_id)}  onChange={handlerChangeCheckBox} />
-    </TableCell>
-    <TableCell ><Link to={`/collection/${_id}`}>{fullName}</Link></TableCell>
-    <TableCell>{email}</TableCell>
-    <TableCell>{position}</TableCell>
-    <TableCell>{status}</TableCell>
-  </TableRow>)}</React.Fragment>
-
+    <TableRow key={uuidv4()}>
+      <TableCell component="th" scope="row">
+        <Checkbox id={_id} checked={isCheck.includes(_id)} onChange={handlerChangeCheckBox} />
+      </TableCell>
+      <TableCell ><Link to={`/collection/${_id}`}>{fullName}</Link></TableCell>
+      <TableCell>{email}</TableCell>
+      <TableCell>{position}</TableCell>
+      <TableCell>{status}</TableCell>
+    </TableRow>
   )
 }
