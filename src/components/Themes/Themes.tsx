5ed5fe44ae7c4c276e523a210d56b7c9437e880a
PiachkoAliaksey <alek17y@mail.ru>
2023-07-22 19:15:39 +0300

    add: translatation functionality for themes cillection

--- src/components/Themes/Themes.tsx
+++ src/components/Themes/Themes.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+
+
 import { THEMES } from "../../constant/themes";
 
 
 export const Themes = () => {
+  const { i18n, t } = useTranslation();
 
   return (
     <>
-      {THEMES.map((item, index) => <option key={item} value={item}>{item}</option>)}
+      {THEMES.map((item, index) => <option key={item} value={item}>{t(`${item}`)}</option>)}
     </>
   )
 }
